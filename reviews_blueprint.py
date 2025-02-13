c53ce384f2218df33fe258fa646fc8378aa95730
TamerlanMustafayev <tamerlan.m.2000@gmail.com>
2025-02-12 19:42:58 -0500

    add delete functionality to reviews blueprint

--- reviews_blueprint.py
+++ reviews_blueprint.py
@@ -53,3 +53,46 @@ def update_review(book_id, review_id):
         return jsonify({"review": updated_review}), 200
     except Exception as error:
         return jsonify({"error": str(error)}), 500
+
+@reviews_blueprint.route('/books/<book_id>/reviews/<review_id>', methods=['DELETE'])
+@token_required
+def delete_review(book_id, review_id):
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+
+        cursor.execute("""
+                        DELETE FROM reviews
+                        WHERE id = %s AND book_id = %s
+                        RETURNING *
+                        """,
+                        (review_id, book_id)
+        )
+        deleted_review = cursor.fetchone()
+        connection.commit()
+        connection.close()
+        return jsonify({"review": "Review deleted successfully"}), 200
+    except Exception as error:
+        return jsonify({"error": str(error)}), 500
+    
+
+@reviews_blueprint.route('/books/<book_id>/reviews', methods=['GET'])
+@token_required
+def get_reviews(book_id):
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+
+        cursor.execute("""
+                        SELECT reviews.*, users.username AS reviewer_username
+                        FROM reviews
+                        JOIN users ON reviews.user_id = users.id
+                        WHERE book_id = %s
+                        """,
+                        (book_id,)
+        )
+        reviews = cursor.fetchall()
+        connection.close()
+        return jsonify({"reviews": reviews}), 200
+    except Exception as error:
+        return jsonify({"error": str(error)}), 500
